4cf06bcb5b7ac4cd560c947f4a33b9e051a7e95d
Agis Tri Wahyuji <agis.vendor.pasti@pegadaian.co.id>
2024-11-18 20:55:49 +0700

    feat(index): middleware, error handling and logger with morgan

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-// const express = require('express')
-
-// const app = express()
-// const port = 3001
-
-// app.get('/', (req, res) => {
-//   res.send({
-//     message: 'Hallo 👋',
-//     status: 'Server ready 🚀',
-//   })
-// })
-
-// app.listen(port, () => {
-//   console.log(`Server ready listening on port ${port}`)
-// })
-
-
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = process.env.PORT || 3001
+
+
+app.use(morgan('combined'))
+
+//custom logger
+// app.use(morgan(function(tokens, req, res){
+//   return [
+//     tokens.method(req, res),
+//     tokens.url(req, res),
+//     tokens.status(req, res),
+//     tokens.res(req, res, 'content-length'), '-',
+//     tokens['response-time'](req, res), 'ms'
+//   ].join(' ')
+// }))
+
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hallo 👋',
+    status: 'Server ready 🚀',
+  })
+})
+
+//route handler that sends the response
+app.get('/blog', (req, res) => {
+  res.send("hello world, blog")
+})
+
+app.listen(port, () => {
+  console.log(`Server ready listening on https://localhost:${port}`)
+})
 
 // const blogs = require('./blog.js')
 
@@ -30,11 +44,12 @@ const port = process.env.PORT || 3001
 //   res.send("you just called the post method at '/hello'! \n")
 // })
 
-app.get('/blog/:title/:id',(req,res) =>{
-  res.send("id:"+ req.params.id + " and title: " + req.params.title)
-})
+// app.get('/blog/:title/:id',(req,res) =>{
+//   res.send("id:"+ req.params.id + " and title: " + req.params.title)
+// })
 
-app.listen(port, () => {
-  console.log(`Running away on ${port}`)
-})
-
+// middleware function to log request protocol
+// app.use('/blog', (req, res, next) => {
+//   console.log("A request for blogs received at", + Date.now())
+//   next()
+// })
