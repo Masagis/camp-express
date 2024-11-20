3f9ac4c1abb1a4d3e2f3916852ba8c2e5682ae14
Agis Tri Wahyuji <agis.vendor.pasti@pegadaian.co.id>
2024-11-20 21:01:04 +0700

    feat(db): integrasi db, migration, and seeding express

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const port = process.env.PORT || 3001
+// const express = require('express')
+// const morgan = require('morgan')
+// const app = express()
+// const port = process.env.PORT || 3001
 
+// app.use(morgan('combined'))
 
-app.use(morgan('combined'))
+// //custom logger
+// // app.use(morgan(function(tokens, req, res){
+// //   return [
+// //     tokens.method(req, res),
+// //     tokens.url(req, res),
+// //     tokens.status(req, res),
+// //     tokens.res(req, res, 'content-length'), '-',
+// //     tokens['response-time'](req, res), 'ms'
+// //   ].join(' ')
+// // }))
 
-//custom logger
-// app.use(morgan(function(tokens, req, res){
-//   return [
-//     tokens.method(req, res),
-//     tokens.url(req, res),
-//     tokens.status(req, res),
-//     tokens.res(req, res, 'content-length'), '-',
-//     tokens['response-time'](req, res), 'ms'
-//   ].join(' ')
-// }))
+// app.get('/', (req, res) => {
+//   res.send({
+//     message: 'Hallo 👋',
+//     status: 'Server ready 🚀',
+//   })
+// })
 
-app.get('/', (req, res) => {
-  res.send({
-    message: 'Hallo 👋',
-    status: 'Server ready 🚀',
-  })
-})
+// //route handler that sends the response
+// app.get('/blog', (req, res) => {
+//   res.send("hello world, blog")
+// })
 
-//route handler that sends the response
-app.get('/blog', (req, res) => {
-  res.send("hello world, blog")
-})
-
-app.listen(port, () => {
-  console.log(`Server ready listening on https://localhost:${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Server ready listening on https://localhost:${port}`)
+// })
 
 // const blogs = require('./blog.js')
 
@@ -53,3 +52,42 @@ app.listen(port, () => {
 //   console.log("A request for blogs received at", + Date.now())
 //   next()
 // })
+
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = process.env.PORT || 3001
+
+const { Books } = require('./models')
+const {Film } = require('./models')
+
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hallo 👋',
+    status: 'Server ready 🚀',
+  })
+})
+
+// Contoh endpoint untuk mendapatkan semua data
+app.get('/api/v1/books', async (req, res) => {
+  try {
+    const books = await Books.findAll()
+    res.json({status: 'OK', data: books,})
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+app.get('/api/v1/film', async (req, res) => {
+  try {
+    const film = await Film.findAll()
+    res.json({status: 'OK', data: film,})
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Server ready listening on https://localhost:${port}`)
+})
---
+++ seeders/20241120134744-films.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.sequelize.query(
+      `insert into Films(title,desc,year) values ("Mia Khalifa","Mia", "2024");`
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
