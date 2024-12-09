0503b641299a75b5ff2580060bd3e357fb427af9
Agis Tri Wahyuji <agis.vendor.pasti@pegadaian.co.id>
2024-12-09 13:58:08 +0700

    pp

--- src/models/index.js
+++ src/models/index.js
@@ -10,17 +10,14 @@ const config = require(__dirname + '/../config/config.js')[env]
 const db = {}
 
 let sequelize
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config, {
-    dialectModule: require('pg'),
-  })
+if (config?.use_env_variable) {
+  sequelize = new Sequelize(process.env[config?.use_env_variable], config)
 } else {
   sequelize = new Sequelize(
     config.database,
     config.username,
     config.password,
     config,
-    { dialectModule: require('pg') },
   )
 }
 
