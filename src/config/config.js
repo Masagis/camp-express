ab7a247808b7a32b50b57da74485f92fd681ada9
Agis Tri Wahyuji <agis.vendor.pasti@pegadaian.co.id>
2024-12-09 14:01:21 +0700

    j

--- src/config/config.js
+++ src/config/config.js
@@ -14,7 +14,21 @@ const config = {
       }
     }
 
-  }
+  },
+  production: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+
+  },
 }
 
 module.exports = config;
